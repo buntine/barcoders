f76efa5c060ad387c1a61ed6bec240e68b82c702
Andrew Buntine Of Doom <bunts@hhd.com.au>
2015-11-02 23:50:09 +1100

    Added parse for code39

--- src/sym/code39.rs
+++ src/sym/code39.rs
@@ -1,15 +1,21 @@
 use ::sym::ToASCII;
 
+const VALID_CHARS: [char; 10] = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9'];
+
 pub struct Code39 {
     pub data: String,
 }
 
 impl Code39 {
+    pub fn parse(data: String) -> Result<Code39, String> {
+        let bad_chars: Vec<char> = data.chars().filter(|c| !VALID_CHARS.binary_search(&c).is_ok() ).collect();
 
-    pub fn parse<'a>(data: String) -> Result<Code39, &'a str> {
-        Ok(Code39{data: data})
+        if bad_chars.len() > 0 {
+            Err(format!("Invalid characters: {:?}", bad_chars))
+        } else {
+            Ok(Code39{data: data})
+        }
     }
-
 }
 
 impl ToASCII for Code39 {
--- src/sym/upca.rs
+++ src/sym/upca.rs
@@ -1,15 +1,15 @@
 use ::sym::ToASCII;
 
+const VALID_CHARS: [char; 10] = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9'];
+
 pub struct UPCA {
     pub data: String,
 }
 
 impl UPCA {
-
-    pub fn parse<'a>(data: String) -> Result<UPCA, &'a str> {
+    pub fn parse(data: String) -> Result<UPCA, String> {
         Ok(UPCA{data: data})
     }
-
 }
 
 impl ToASCII for UPCA {
