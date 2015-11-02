de649a57875ef399251f586374851f935006c2cc
Andrew Buntine Of Doom <bunts@hhd.com.au>
2015-11-02 22:44:46 +1100

    Initial test passing

--- src/lib.rs
+++ src/lib.rs
@@ -1,3 +1,12 @@
-#[test]
-fn it_works() {
+pub mod sym;
+
+mod tests {
+    use super::sym::*;
+
+    #[test]
+    fn new_upca() {
+        let upca = UPCA::new("123412".to_string());
+
+        assert_eq!(upca.data, "123412".to_string());
+    }
 }
---
+++ src/sym/mod.rs
@@ -0,0 +1,11 @@
+pub struct UPCA {
+    pub data: String,
+}
+
+impl UPCA {
+
+    pub fn new(data: String) -> UPCA {
+        UPCA{data: data}
+    }
+
+}
