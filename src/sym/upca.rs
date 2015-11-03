b816cb62a5aac7d45c32d104613ecfd6d57ed6c6
Andrew Buntine Of Doom <bunts@hhd.com.au>
2015-11-03 20:13:53 +1100

    Starting to add encodings map

--- src/sym/upca.rs
+++ src/sym/upca.rs
@@ -3,14 +3,44 @@ use ::sym::Parse;
 use std::ops::Range;
 use std::char;
 
+pub const ENCODINGS: [[&'static str; 10]; 2] = [
+    [
+     "0001101",
+     "0000000",
+     "0000000",
+     "0000000",
+     "0000000",
+     "0000000",
+     "0000000",
+     "0000000",
+     "0000000",
+     "0000000",
+    ],
+    [
+     "1110010",
+     "0000000",
+     "0000000",
+     "0000000",
+     "0000000",
+     "0000000",
+     "0000000",
+     "0000000",
+     "0000000",
+     "0000000",
+    ],
+];
+
 pub struct UPCA {
     data: String,
+    pub xdim: u32,
+    pub ydim: u32,
+    pub height: u32,
 }
 
 impl UPCA {
     pub fn new(data: String) -> Result<UPCA, String> {
         match UPCA::parse(data) {
-            Ok(d) => Ok(UPCA{data: d}),
+            Ok(d) => Ok(UPCA{data: d, xdim: 1, ydim: 1, height: 100}),
             Err(e) => Err(e),
         }
     }
