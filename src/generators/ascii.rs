976f67cf23c51f839a269776d2aa440cf2a37631
Andrew Buntine von Doom <info@bunts.io>
2015-11-12 15:11:41 +1100

    Added tests for ASCII

--- src/generators/ascii.rs
+++ src/generators/ascii.rs
@@ -1,39 +1,95 @@
 use ::sym::EncodedBarcode;
+use std::iter::repeat;
 
 pub struct ASCII {
-    pub height: u32,
-    pub xdim: u32,
+    pub height: usize,
+    pub xdim: usize,
 }
+
+pub const ASCII_CHARS: [char; 2] = [' ', '#'];
 
 impl ASCII {
     pub fn new() -> ASCII {
         ASCII{height: 10, xdim: 1}
     }
 
-    pub fn height(mut self, h: u32) -> ASCII {
+    pub fn height(mut self, h: usize) -> ASCII {
         self.height = h;
         self
     }
 
-    pub fn xdim(mut self, x: u32) -> ASCII {
+    pub fn xdim(mut self, x: usize) -> ASCII {
         self.xdim = x;
         self
     }
 
     fn generate_row(&self, barcode: &EncodedBarcode) -> String {
-        "##  ".to_string()
+        barcode.iter()
+               .flat_map(|&d| repeat(ASCII_CHARS[d as usize]).take(self.xdim))
+               .collect()
     }
 
-    // TODO: Implement.
     pub fn generate(&self, barcode: &EncodedBarcode) -> Result<String, String> {
         let mut output = String::new();
         let row = self.generate_row(&barcode);
 
-        for _l in 0..self.height {
+        for (i, _l) in (0..self.height).enumerate() {
             output.push_str(&row[..]);
-            output.push_str("\n");
+
+            if i < self.height - 1 {
+                output.push_str("\n");
+            }
         }
 
         Ok(output)
     }
 }
+
+#[cfg(test)]
+mod tests {
+    use ::sym::ean13::*;
+    //use ::sym::ean8::*;
+   // use ::sym::code39::*;
+    use ::generators::ascii::*;
+    use ::sym::Encode;
+
+    #[test]
+    fn ean_13_as_ascii() {
+        let ean13 = EAN13::new("750103131130".to_string()).unwrap();
+        let ascii = ASCII::new();
+        let generated = ascii.generate(&ean13.encode()).unwrap();
+
+        assert_eq!(generated,
+"
+# # ##   # #  ###  ##  # #  ### #### # ##  ## # # #    # ##  ## ##  ## #    # ###  # ### #  # #
+# # ##   # #  ###  ##  # #  ### #### # ##  ## # # #    # ##  ## ##  ## #    # ###  # ### #  # #
+# # ##   # #  ###  ##  # #  ### #### # ##  ## # # #    # ##  ## ##  ## #    # ###  # ### #  # #
+# # ##   # #  ###  ##  # #  ### #### # ##  ## # # #    # ##  ## ##  ## #    # ###  # ### #  # #
+# # ##   # #  ###  ##  # #  ### #### # ##  ## # # #    # ##  ## ##  ## #    # ###  # ### #  # #
+# # ##   # #  ###  ##  # #  ### #### # ##  ## # # #    # ##  ## ##  ## #    # ###  # ### #  # #
+# # ##   # #  ###  ##  # #  ### #### # ##  ## # # #    # ##  ## ##  ## #    # ###  # ### #  # #
+# # ##   # #  ###  ##  # #  ### #### # ##  ## # # #    # ##  ## ##  ## #    # ###  # ### #  # #
+# # ##   # #  ###  ##  # #  ### #### # ##  ## # # #    # ##  ## ##  ## #    # ###  # ### #  # #
+# # ##   # #  ###  ##  # #  ### #### # ##  ## # # #    # ##  ## ##  ## #    # ###  # ### #  # #
+".trim().to_string());
+    }
+
+    #[test]
+    fn ean_13_as_ascii_small_height_double_width() {
+        let ean13 = EAN13::new("750103131130".to_string()).unwrap();
+        let ascii = ASCII::new().height(6).xdim(2);
+        let generated = ascii.generate(&ean13.encode()).unwrap();
+
+        assert_eq!(generated,
+"
+##  ##  ####      ##  ##    ######    ####    ##  ##    ######  ########  ##  ####    ####  ##  ##  ##        ##  ####    ####  ####    ####  ##        ##  ######    ##  ######  ##    ##  ##
+##  ##  ####      ##  ##    ######    ####    ##  ##    ######  ########  ##  ####    ####  ##  ##  ##        ##  ####    ####  ####    ####  ##        ##  ######    ##  ######  ##    ##  ##
+##  ##  ####      ##  ##    ######    ####    ##  ##    ######  ########  ##  ####    ####  ##  ##  ##        ##  ####    ####  ####    ####  ##        ##  ######    ##  ######  ##    ##  ##
+##  ##  ####      ##  ##    ######    ####    ##  ##    ######  ########  ##  ####    ####  ##  ##  ##        ##  ####    ####  ####    ####  ##        ##  ######    ##  ######  ##    ##  ##
+##  ##  ####      ##  ##    ######    ####    ##  ##    ######  ########  ##  ####    ####  ##  ##  ##        ##  ####    ####  ####    ####  ##        ##  ######    ##  ######  ##    ##  ##
+##  ##  ####      ##  ##    ######    ####    ##  ##    ######  ########  ##  ####    ####  ##  ##  ##        ##  ####    ####  ####    ####  ##        ##  ######    ##  ######  ##    ##  ##
+".trim().to_string());
+    }
+
+}
+ 
