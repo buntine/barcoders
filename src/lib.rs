ec9ef8941dfe1a2c3cd688cc465af731244dc2c7
Pascal Hertleif <killercup@gmail.com>
2015-11-23 11:09:04 +0100

    Enable More Lints
    I'll let you off with a warning, this time.

--- src/lib.rs
+++ src/lib.rs
@@ -34,6 +34,13 @@
 //! * JPEG (feature: `image`)
 //! * More coming! (PostScript, SVG, etc)
 
+#![warn(missing_docs,
+        missing_debug_implementations, missing_copy_implementations,
+        trivial_casts, trivial_numeric_casts,
+        unsafe_code,
+        unstable_features,
+        unused_import_braces, unused_qualifications)]
+
 #[cfg(feature = "image")]
 extern crate image;
 
