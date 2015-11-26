5931d78fc48164533c6ce854f8bdc016e8e51960
Andrew Buntine Of Doom <bunts@hhd.com.au>
2015-11-27 00:03:09 +1100

    Changed interface for encoding images to return Vec<u8> of bytes

--- src/generators/image.rs
+++ src/generators/image.rs
@@ -77,8 +77,8 @@ impl Image {
         }
     }
 
-    /// Generates the given barcode. Returns a `Result<usize, &str>` indicating the number of bytes written.
-    pub fn generate(&self, barcode: &[u8], path: &mut File) -> Result<usize, &str> {
+    /// Generates the given barcode. Returns a `Result<Vec<u8>, &str>` of the encoded bytes.
+    pub fn generate(&self, barcode: &[u8]) -> Result<Vec<u8>, &str> {
         let (xdim, height, format) = match *self {
             Image::GIF{height: h, xdim: x} => (x, h, image::GIF),
             Image::PNG{height: h, xdim: x} => (x, h, image::PNG),
@@ -105,12 +105,13 @@ impl Image {
             pos = 0;
         }
 
-        let buflen = buffer.len();
+        let mut bytes: Vec<u8> = vec![];
 
-        match image::ImageLuma8(buffer).save(path, format) {
-            Ok(_) => Ok(buflen),
-            _ => Err("Could not encode image."),
+        {
+            image::ImageLuma8(buffer).save(&mut bytes, format).unwrap();
         }
+
+        Ok(bytes)
     }
 }
 
@@ -124,233 +125,213 @@ mod tests {
     use ::sym::ean_supp::*;
     use ::sym::tf::*;
     use ::generators::image::*;
+    use std::io;
+    use std::io::prelude::*;
+    use std::io::BufWriter;
     use std::fs::File;
     use std::path::Path;
 
     const TEST_DATA_BASE: &'static str = "./target/debug";
+    const WRITE_TO_FILE: bool = true;
 
     fn open_file(name: &'static str) -> File {
         File::create(&Path::new(&format!("{}/{}", TEST_DATA_BASE, name)[..])).unwrap()
     }
 
+    fn write_file(bytes: &[u8], file: &'static str) {
+        let path = open_file(file);
+        let mut writer = BufWriter::new(path);
+        writer.write(bytes).unwrap();
+    }
+
     #[test]
     fn ean_13_as_gif() {
-        let mut path = open_file("ean13.gif");
-
         let ean13 = EAN13::new("750103131130".to_owned()).unwrap();
         let gif = Image::gif();
-        let generated = gif.generate(&ean13.encode()[..], &mut path).unwrap();
+        let generated = gif.generate(&ean13.encode()[..]).unwrap();
 
-        assert_eq!(generated, 7600);
+        if WRITE_TO_FILE { write_file(&generated[..], "ean13.gif"); }
+
+        assert_eq!(generated.len(), 7600);
     }
 
     #[test]
     fn ean_13_as_png() {
-        let mut path = open_file("ean13.png");
-
         let ean13 = EAN13::new("750103131130".to_owned()).unwrap();
         let png = Image::PNG {
             height: 100,
             xdim: 1,
         };
-        let generated = png.generate(&ean13.encode()[..], &mut path).unwrap();
+        let generated = png.generate(&ean13.encode()[..]).unwrap();
 
-        assert_eq!(generated, 9500);
+        assert_eq!(generated.len(), 9500);
     }
 
     #[test]
     fn ean_13_as_jpeg() {
-        let mut path = open_file("ean13.jpg");
-
         let ean13 = EAN13::new("999988881234".to_owned()).unwrap();
         let jpeg = Image::JPEG {
             height: 100,
             xdim: 3,
         };
-        let generated = jpeg.generate(&ean13.encode()[..], &mut path).unwrap();
+        let generated = jpeg.generate(&ean13.encode()[..]).unwrap();
 
-        assert_eq!(generated, 28500);
+        assert_eq!(generated.len(), 28500);
     }
 
     #[test]
     fn code39_as_png() {
-        let mut path = open_file("code39.png");
-
         let code39 = Code39::new("ILOVEMEL".to_owned()).unwrap();
         let png = Image::PNG {
             height: 60,
             xdim: 1,
         };
-        let generated = png.generate(&code39.encode()[..], &mut path).unwrap();
+        let generated = png.generate(&code39.encode()[..]).unwrap();
 
-        assert_eq!(generated, 7740);
+        assert_eq!(generated.len(), 7740);
     }
 
     #[test]
     fn code39_as_gif() {
-        let mut path = open_file("code39.gif");
-
         let code39 = Code39::new("WIKIPEDIA".to_owned()).unwrap();
         let gif = Image::GIF {
             height: 60,
             xdim: 1,
         };
-        let generated = gif.generate(&code39.encode()[..], &mut path).unwrap();
+        let generated = gif.generate(&code39.encode()[..]).unwrap();
 
-        assert_eq!(generated, 8520);
+        assert_eq!(generated.len(), 8520);
     }
 
     #[test]
     fn code39_as_jpeg() {
-        let mut path = open_file("code39.jpg");
-
         let code39 = Code39::new("SWAGLORDTHE3RD".to_owned()).unwrap();
         let jpeg = Image::JPEG {
             height: 160,
             xdim: 1,
         };
-        let generated = jpeg.generate(&code39.encode()[..], &mut path).unwrap();
+        let generated = jpeg.generate(&code39.encode()[..]).unwrap();
 
-        assert_eq!(generated, 33120);
+        assert_eq!(generated.len(), 33120);
     }
 
     #[test]
     fn ean8_as_png() {
-        let mut path = open_file("ean8.png");
-
         let ean8 = EAN8::new("5512345".to_owned()).unwrap();
         let png = Image::PNG {
             height: 70,
             xdim: 2,
         };
-        let generated = png.generate(&ean8.encode()[..], &mut path).unwrap();
+        let generated = png.generate(&ean8.encode()[..]).unwrap();
 
-        assert_eq!(generated, 9380);
+        assert_eq!(generated.len(), 9380);
     }
 
     #[test]
     fn ean8_as_gif() {
-        let mut path = open_file("ean8.gif");
-
         let ean8 = EAN8::new("9992227".to_owned()).unwrap();
         let gif = Image::GIF {
             height: 70,
             xdim: 2,
         };
-        let generated = gif.generate(&ean8.encode()[..], &mut path).unwrap();
+        let generated = gif.generate(&ean8.encode()[..]).unwrap();
 
-        assert_eq!(generated, 9380);
+        assert_eq!(generated.len(), 9380);
     }
 
     #[test]
     fn ean8_as_jpeg() {
-        let mut path = open_file("ean8.jpg");
-
         let ean8 = EAN8::new("9992227".to_owned()).unwrap();
         let jpeg = Image::JPEG {
             height: 70,
             xdim: 2,
         };
-        let generated = jpeg.generate(&ean8.encode()[..], &mut path).unwrap();
+        let generated = jpeg.generate(&ean8.encode()[..]).unwrap();
 
-        assert_eq!(generated, 9380);
+        assert_eq!(generated.len(), 9380);
     }
 
     #[test]
     fn ean2_as_png() {
-        let mut path = open_file("ean2.png");
-
         let ean2 = EANSUPP::new("94".to_owned()).unwrap();
         let png = Image::PNG {
             height: 70,
             xdim: 2,
         };
-        let generated = png.generate(&ean2.encode()[..], &mut path).unwrap();
+        let generated = png.generate(&ean2.encode()[..]).unwrap();
 
-        assert_eq!(generated, 2800);
+        assert_eq!(generated.len(), 2800);
     }
 
     #[test]
     fn ean5_as_gif() {
-        let mut path = open_file("ean5.gif");
-
         let ean5 = EANSUPP::new("51234".to_owned()).unwrap();
         let gif = Image::GIF {
             height: 70,
             xdim: 2,
         };
-        let generated = gif.generate(&ean5.encode()[..], &mut path).unwrap();
+        let generated = gif.generate(&ean5.encode()[..]).unwrap();
 
-        assert_eq!(generated, 6580);
+        assert_eq!(generated.len(), 6580);
     }
 
     #[test]
     fn ean5_as_jpeg() {
-        let mut path = open_file("ean5.jpg");
-
         let ean5 = EANSUPP::new("51574".to_owned()).unwrap();
         let jpeg = Image::JPEG {
             height: 140,
             xdim: 5,
         };
-        let generated = jpeg.generate(&ean5.encode()[..], &mut path).unwrap();
+        let generated = jpeg.generate(&ean5.encode()[..]).unwrap();
 
-        assert_eq!(generated, 32900);
+        assert_eq!(generated.len(), 32900);
     }
 
     #[test]
     fn itf_as_png() {
-        let mut path = open_file("itf.png");
-
         let itf = TF::interleaved("1234567".to_owned()).unwrap();
         let png = Image::PNG {
             height: 100,
             xdim: 2,
         };
-        let generated = png.generate(&itf.encode()[..], &mut path).unwrap();
+        let generated = png.generate(&itf.encode()[..]).unwrap();
 
-        assert_eq!(generated, 16000);
+        assert_eq!(generated.len(), 16000);
     }
 
     #[test]
     fn stf_as_png() {
-        let mut path = open_file("stf.png");
-
         let stf = TF::standard("1234567".to_owned()).unwrap();
         let png = Image::PNG {
             height: 100,
             xdim: 2,
         };
-        let generated = png.generate(&stf.encode()[..], &mut path).unwrap();
+        let generated = png.generate(&stf.encode()[..]).unwrap();
 
-        assert_eq!(generated, 22800);
+        assert_eq!(generated.len(), 22800);
     }
 
     #[test]
     fn itf_as_gif() {
-        let mut path = open_file("itf.gif");
-
         let itf = TF::interleaved("98766543561".to_owned()).unwrap();
         let gif = Image::GIF {
             height: 130,
             xdim: 1,
         };
-        let generated = gif.generate(&itf.encode()[..], &mut path).unwrap();
+        let generated = gif.generate(&itf.encode()[..]).unwrap();
 
-        assert_eq!(generated, 15080);
+        assert_eq!(generated.len(), 15080);
     }
 
     #[test]
     fn itf_as_jpeg() {
-        let mut path = open_file("itf.jpg");
-
         let itf = TF::interleaved("98766543561".to_owned()).unwrap();
         let jpeg = Image::JPEG {
             height: 130,
             xdim: 1,
         };
-        let generated = jpeg.generate(&itf.encode()[..], &mut path).unwrap();
+        let generated = jpeg.generate(&itf.encode()[..]).unwrap();
 
-        assert_eq!(generated, 15080);
+        assert_eq!(generated.len(), 15080);
     }
 }
