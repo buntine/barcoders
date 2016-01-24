fc606416ec1dbf4c5758287d96b9adaf63a83300
Andrew Buntine Of Doom <bunts@hhd.com.au>
2016-01-24 23:03:47 +1100

    Coded myself into a fuckin circle of hell. Try again tomorrow

--- src/sym/code128.rs
+++ src/sym/code128.rs
@@ -12,6 +12,10 @@
 //!   \b => Switch to character-set B
 //!   \c => Switch to character-set C
 //!
+//! You must provide both the starting character-set along with any changes during the data. This
+//! means all Code128 barcodes must start with either "\a", "\b" or "\c". Simple alphanumeric data
+//! can generally use character-set A solely.
+//!
 //! As an example, this barcode uses character-set B:
 //!   \bHE1234A*1
 //!
@@ -24,28 +28,28 @@
 //!   \a1234\\45AA
 
 use sym::helpers;
-use error::Result;
+use error::*;
 
 #[derive(Debug, PartialEq, Eq)]
 pub enum Unit {
-    A(String),
-    B(String),
-    C(String),
+    A(u8),
+    B(u8),
+    C(u8),
 }
 
-// Character -> Binary mappings for each of the allowable characters in character-set A.
-const CODE128_CHARS_A: [(&'static str, [u8; 11]); 3] = [
-    ("0", [1,0,1,0,0,1,1,0,1,1,0]), ("1", [1,1,0,1,0,0,1,0,1,0,1]), ("2", [1,0,1,1,0,0,1,0,1,0,1]),
-];
+type Encoding = [u8; 11];
 
-// Character -> Binary mappings for each of the allowable characters in character-set B.
-const CODE128_CHARS_B: [(&'static str, [u8; 11]); 3] = [
-    ("A", [1,0,1,0,0,1,1,0,1,1,0]), ("B", [1,1,0,1,0,0,1,0,1,0,1]), ("C", [1,0,1,1,0,0,1,0,1,0,1]),
-];
+pub enum UnitType {
+    A,
+    B,
+    C,
+}
 
-// Character -> Binary mappings for each of the allowable characters in character-set C.
-const CODE128_CHARS_C: [(&'static str, [u8; 11]); 3] = [
-    ("00", [1,0,1,0,0,1,1,0,1,1,0]), ("01", [1,1,0,1,0,0,1,0,1,0,1]), ("02", [1,0,1,1,0,0,1,0,1,0,1]),
+// Character -> Binary mappings for each of the allowable characters in each character-set.
+const CODE128_CHARS: [([&'static str; 3], Encoding); 3] = [
+    ([" ", " ", "00"], [1,0,1,0,0,1,1,0,1,1,0]), 
+    (["!", "!", "01"], [1,1,1,0,0,1,1,0,1,1,0]), 
+    (["\"", "\"", "02"], [1,1,1,1,0,1,1,0,1,1,0]), 
 ];
 
 /// The Code128 barcode type.
@@ -64,31 +68,77 @@ impl Code128 {
 
     // Tokenizes and collects the data into the appropriate character-sets.
     fn parse(chars: Vec<char>) -> Result<Vec<Unit>> {
-        Ok(vec![Unit::A("1".to_string()), Unit::A("2".to_string())])
-    }
+//        let mut units: Vec<Encoding> = vec![];
+//        let mut control_char = false;
+//        let mut unit_type: Option<UnitType> = None;
+//        let mut carry: Option<u32> = None;
+//
+//        for ch in chars {
+//            match ch {
+//                '\\' => {
+//                    carry = None;
+//
+//                    if control_char {
+//                        match unit_type {
+//                            Some(ut) => {
+//                                let b = try!(Code128::unit_encoding("\\", ut));
+//                                units.push(b);
+//                            },
+//                            None => return Err(Error::Character('\\'))
+//                        }
+//                    } else {
+//                        control_char = true;
+//                    }
+//                },
+//                d if d.is_digit(10) => {
+//                    match carry {
+//                        Some(c) if unit_type == UnitType::C => {
+//                            units.pop();
+//
+//                            let b = try!(Code128::unit_encoding("00", ut));
+//                            units.push(b);
+//                        },
+//                        _ => {
+//
+//                        }
+//                    }
+//                },
+//                'a' if control_char && (unit_type == Some(UnitType::B) || unit_type == Some(UnitType::C)) => {
+//                }
+//                'b' if control_char && (unit_type == Some(UnitType::A) || unit_type == Some(UnitType::C)) => {
+//                }
+//                'c' if control_char && (unit_type == Some(UnitType::A) || unit_type == Some(UnitType::B)) => {
+//                }
+//                _ => {
+//                    carry = None;
+//
+//
+//                }
+//            }
+//        }
+//
+//        units
 
-    /// Returns the tokenized data as was passed into the constructor.
-    pub fn raw_data(&self) -> &[Unit] {
-        &self.0
+        Ok(vec![Unit::A(0)])
     }
 
     /// Calculates the checksum unit using a modulo-103 algorithm.
-    pub fn checksum_unit(&self) -> Option<Unit> {
-        Some(Unit::C("23".to_string()))
+    pub fn checksum_value(&self) -> Option<u32> {
+        Some(23)
     }
 
-    fn checksum_encoding(&self) -> [u8; 11] {
-        match self.checksum_unit() {
-            Some(u) => self.unit_encoding(&u),
+    fn checksum_encoding(&self) -> Encoding {
+        match self.checksum_value() {
+            Some(u) => self.unit_encoding(&Unit::A(u as u8)),
             None => panic!("Cannot compute checksum"),
         }
     }
 
-    fn unit_encoding(&self, c: &Unit) -> [u8; 11] {
+    fn unit_encoding(&self, c: &Unit) -> Encoding {
         [1,1,1,0,0,0,1,1,1,0,0]
     }
 
-    fn push_encoding(&self, into: &mut Vec<u8>, from: [u8; 11]) {
+    fn push_encoding(&self, into: &mut Vec<u8>, from: Encoding) {
         into.extend(from.iter().cloned());
         into.push(0);
     }
@@ -143,11 +193,11 @@ mod tests {
 //
 //        assert_eq!(code128.err().unwrap(), Error::Length);
 //    }
-
-    #[test]
-    fn code128_raw_data() {
-        let code128 = Code128::new("12001".to_owned()).unwrap();
-
-        assert_eq!(code128.raw_data(), &[Unit::A("1".to_string()), Unit::A("2".to_string())]);
-    }
+//
+//    #[test]
+//    fn code128_raw_data() {
+//        let code128 = Code128::new("12001".to_owned()).unwrap();
+//
+//        assert_eq!(code128.raw_data(), &[Unit::A("1".to_string()), Unit::A("2".to_string())]);
+//    }
 }
