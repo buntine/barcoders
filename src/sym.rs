3d9fbf1e738964fbbe328e3f23802cfaec17b121
AtomicGamer9523 <drunkenmonkey4987@gmail.com>
2024-06-04 15:22:14 -0400

    Added dev utilities

--- src/sym.rs
+++ src/sym.rs
@@ -23,3 +23,45 @@ pub use code11::Code11;
 // 
 // mod tf;
 // pub use tf::TF;
+
+/// An extension trait for barcode symbologies.
+/// 
+/// This trait provides a default implementation for the `validate` method
+/// commonly used in the `new` method of barcode symbologies.
+pub trait BarcodeDevExt<'a>: Barcode<'a> {
+    /// Performs validation on the data.
+    /// 
+    /// Users of the library should not need to call this method directly.
+    /// This method is provided for potential implementors of new barcode symbologies.
+    fn validate(data: &'a [u8]) -> Result<&'a [u8]> {
+        let len = data.len() as u16;
+        if len < Self::SIZE.start || len > Self::SIZE.end {
+            return Err(error::Error::Length);
+        }
+        for &byte in data.iter() {
+            if !Self::CHARS.contains(&byte) {
+                return Err(error::Error::Character);
+            }
+        }
+        Ok(data)
+    }
+}
+
+#[macro_export(local_inner_macros)]
+macro_rules! encode {
+    (($buffer:ident, $i:ident) $v:ident {
+        $($pat:pat => [$($bit:literal),+],)+
+    }) => (
+        match $v {
+            $($pat => {
+                $(
+                    $buffer[$i] = $bit;
+                    $i += 1;
+                )+
+            },)*
+            _ => ::core::unreachable!("Validation did not catch an illegal character"),
+        }
+    )
+}
+
+impl<'a, B> BarcodeDevExt<'a> for B where B: Barcode<'a> {}
